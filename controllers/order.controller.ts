5f3d761b19ba48540a2755d48cbd5a7027e75783
ianPOC <Ian.Campos@encora.com>
2023-12-05 16:35:13 -0300

    order view config

--- controllers/order.controller.ts
+++ controllers/order.controller.ts
@@ -1,44 +1,54 @@
 const path = require('path');
 const Order = require('../models/order');
+const Snack = require('../models/snack');
 
-exports.goToMakeOrderPage = (req: any, res: any, next: any) => {
+exports.goToMakeOrderPage = async (req: any, res: any, next: any) => {
     console.log('MAKE ORDER PAGE');
-    res.render('make-order');
-};
 
-exports.addIngredient = (req: any, res: any, next: any) => {
-    console.log('Add INGREDIENT POST');
-    console.log('BODY', req.body)
-    const snackId = req.params.id;
-    const name = req.body.ingredient_name;
-    const quantity = req.body.ingredient_quantity;
-    
-    Order.create({
-        name: name,
-        quantity: quantity,
-        snackId: snackId
-    })
-    .then((result: any) => {
-        console.log(result)
-        res.redirect('/ingredients');
-    })
-    .catch((err: any) => {
-        console.log(err);
-        res.status(500).send(`Error while adding ingredient ${name}.`);
-    })
-};
-
-exports.getIngredients = async (req: any, res: any, next: any) => {
-    console.log('GET INGREDIENTS');
-    await Order.findAll()
+    await Snack.findAll()
     .then((rows: any) => {
-        res.render('ingredients', {
-            ingredients: rows,
+        res.render('make-order', {
+            snacks: rows,
         });
     })
     .catch((err: any) => {
         console.error(err);
-        res.status(500).send('Error while getting ingredients.');
+        res.status(500).send('Error while getting snacks at order page.');
     });
 };
 
+// exports.addIngredient = (req: any, res: any, next: any) => {
+//     console.log('Add INGREDIENT POST');
+//     console.log('BODY', req.body)
+//     const snackId = req.params.id;
+//     const name = req.body.ingredient_name;
+//     const quantity = req.body.ingredient_quantity;
+    
+//     Order.create({
+//         name: name,
+//         quantity: quantity,
+//         snackId: snackId
+//     })
+//     .then((result: any) => {
+//         console.log(result)
+//         res.redirect('/ingredients');
+//     })
+//     .catch((err: any) => {
+//         console.log(err);
+//         res.status(500).send(`Error while adding ingredient ${name}.`);
+//     })
+// };
+
+// exports.getIngredients = async (req: any, res: any, next: any) => {
+//     console.log('GET INGREDIENTS');
+//     await Order.findAll()
+//     .then((rows: any) => {
+//         res.render('ingredients', {
+//             ingredients: rows,
+//         });
+//     })
+//     .catch((err: any) => {
+//         console.error(err);
+//         res.status(500).send('Error while getting ingredients.');
+//     });
+// };
